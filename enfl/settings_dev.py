0acbf506d61177fc4960b688e754f04c3d5a8f40
Rich Barton-Cooper <rich@duffel.com>
2023-07-07 22:05:58 +0100

    Add localhost to ALLOWED_HOSTS

--- enfl/settings_dev.py
+++ enfl/settings_dev.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7kad+maz2rcr43iv9!^kr@z(@!dv$vv+j&u%p3rsi!cf2l&@f8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
